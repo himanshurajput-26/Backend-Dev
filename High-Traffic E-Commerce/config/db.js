939371097f57d61fa50e62e840d3ceb6fcbc273b
himanshurajput-26 <himanshurajput261204@gmail.com>
2026-05-01 12:26:21 +0530

    Practical Done

--- High-Traffic E-Commerce/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://your-db-url");
-  console.log("MongoDB Connected");
-};
-
-module.exports = connectDB;
